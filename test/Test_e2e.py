90cd63679036efe2c8a20ae625c640d783f9c846
Gaurav_office <gaurav.kp43@gmail.com>
2023-12-19 13:50:12 +0530

    office Partner portal

---
+++ test/Test_e2e.py
@@ -0,0 +1,100 @@
+import time
+
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+
+
+@pytest.mark.usefixtures('setup')
+class Testone:
+
+    def test_dashboradlanding(self):
+        assert self.driver.title == "Dashboard"
+
+    def test_dashboardcheck(self):
+        self.driver.implicitly_wait(5)
+        date_filter= Select(self.driver.find_element(By.CSS_SELECTOR,"#ChoosePastHistory"))
+        date_filter.select_by_visible_text('Last 3 Months')
+        self.driver.find_element(By.CSS_SELECTOR,"[role='textbox']").click()
+        time.sleep(2)
+        self.driver.find_element(By.CSS_SELECTOR, "[type='search']").send_keys('a')
+        time.sleep(2)
+        variable= self.driver.find_elements(By.XPATH, "//ul/li[@role='option']")
+        for vendor in variable:
+
+            if vendor.text == "Autumn Harp Business Limited":
+                vendor.click()
+                break
+    def test_removeselectedvendor(self):
+        self.driver.find_element(By.CSS_SELECTOR,"button span").click()
+        self.driver.find_element(By.CSS_SELECTOR, "[role='textbox']").click()
+
+
+    def test_dashboardtext(self):
+        message= self.driver.find_element(By.TAG_NAME,'h2').text
+        assert  message == "Good Morning" or message == "Good Afternoon" or message== "Good Evening"
+
+    def test_customoption(self):
+        date_filtercc = Select(self.driver.find_element(By.CSS_SELECTOR, "#ChoosePastHistory"))
+        date_filtercc.select_by_visible_text('Custom Option')
+        self.driver.find_element(By.CSS_SELECTOR,"[name='customDateFilter']").clear()
+        self.driver.find_element(By.CSS_SELECTOR,"[name='customDateFilter']").send_keys("01/14/2024 - 01/25/2024")
+
+
+    def test_customoption_check(self):
+        self.driver.implicitly_wait(5)
+        month_Year_left= "Feb 2023"
+        month_year_right="Sep 2023"
+
+
+        self.driver.find_element(By.CSS_SELECTOR, "[name='customDateFilter']").clear()
+        #time.sleep(1)
+        mon_year = self.driver.find_element(By.CSS_SELECTOR, "div[class='drp-calendar left'] th[class='month']").text
+
+
+        while True:
+           mon_year_left = self.driver.find_element(By.CSS_SELECTOR,
+                                               "div[class='drp-calendar left'] th[class='month']").text
+           print(mon_year)
+           if month_Year_left == mon_year_left:
+               self.driver.find_element(By.XPATH,"//body[1]/div[2]/div[2]/div[1]/table[1]/tbody[1]/tr[3]/td[3]").click()
+               break
+           else:
+               self.driver.find_element(By.XPATH, "//th[@class='prev available']").click()
+
+        while True:
+            mon_year_right= (self.driver.find_element
+                             (By.CSS_SELECTOR, "div[class='drp-calendar right'] th[class='month']").text)
+            if month_year_right== mon_year_right:
+                self.driver.find_element(By.XPATH,"//body[1]/div[2]/div[3]/div[1]/table[1]/tbody[1]/tr[3]/td[4]").click()
+                break
+            else:
+                self.driver.find_element(By.XPATH, "//th[@class='next available']").click()
+
+        self.driver.find_element(By.XPATH, '//button[text()="Apply"]').click()
+
+    def test_vendor_acitivity(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/conftest.py
@@ -0,0 +1,40 @@
+import calendar
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from datetime import datetime
+
+
+@pytest.fixture(scope='class')
+def setup(request):
+    option= webdriver.ChromeOptions()
+    option.add_experimental_option("detach",True)
+    driver= webdriver.Chrome(options=option)
+    driver.get("https://www.partnerportal.ai/")
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    action = ActionChains(driver)
+    action.move_to_element(driver.find_element(By.XPATH, "//a[normalize-space()='Sign In']")).perform()
+    action.move_to_element(driver.find_element(By.CSS_SELECTOR, "[title='Company']")).click().perform()
+    driver.find_element(By.CSS_SELECTOR,"[name='username']").send_keys("vibinguniverse")
+    driver.find_element(By.CSS_SELECTOR, "[name='password']").send_keys("Admin@123")
+    driver.find_element(By.CSS_SELECTOR, "[name='remember']").click()
+    driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+    request.cls.driver= driver
+
+
+
+    current_datetime = datetime.now()
+    current_year = current_datetime.strftime("%Y")
+    #print(current_year)
+    month_num=current_datetime.strftime("%m")
+    month_name = calendar.month_name[int(month_num)]
+    mon_name_year= (month_name[0:3]+" "+current_year)
+    print(mon_name_year)
+    request.cls.mon_name_year= mon_name_year
+
+
+
+
